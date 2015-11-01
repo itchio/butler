86e36745fbbf88afebc93f82349a7ac842f0a48a
Amos Wenger <fasterthanlime@gmail.com>
2015-11-01 17:30:54 +0100

    test-ssh command

--- butler.go
+++ butler.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/crypto/ssh"
 )
 
 var version = "head"
@@ -31,6 +36,8 @@ func main() {
 		fmt.Println(fmt.Sprintf("butler version %s", version))
 	case "dl":
 		dl()
+	case "test-ssh":
+		testSSH()
 	default:
 		err("Invalid command")
 	}
@@ -84,4 +91,52 @@ func dl() {
 			break
 		}
 	}
+}
+
+func publicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+
+	log.Println("Our public key is", base64.StdEncoding.EncodeToString(key.PublicKey().Marshal()))
+	return ssh.PublicKeys(key)
+}
+
+func testSSH() {
+	host := "butler.itch.zone"
+	port := 2222
+	serverString := fmt.Sprintf("%s:%d", host, port)
+	fmt.Printf("Trying to connect to %s\n", serverString)
+
+	keyPath := fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".ssh/id_rsa")
+	key := publicKeyFile(keyPath)
+	auth := []ssh.AuthMethod{key}
+
+	sshConfig := &ssh.ClientConfig{
+		User: "butler",
+		Auth: auth,
+	}
+
+	serverConn, err := ssh.Dial("tcp", serverString, sshConfig)
+	if err != nil {
+		fmt.Printf("Server dial error: %s\n", err)
+		return
+	}
+	fmt.Printf("Connected!\n")
+
+	ch, _, err := serverConn.OpenChannel("butler", []byte{})
+	if err != nil {
+		panic(err)
+	}
+
+	ch.Write([]byte("Hi"))
+	ch.Close()
+
+	serverConn.Close()
 }
