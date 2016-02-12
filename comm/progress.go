805b0ad506766293245943a96d8f9b9e0ab2454b
Amos Wenger <fasterthanlime@gmail.com>
2016-02-12 21:37:32 +0100

    Add unicode progress bar themes instead of fixing bugs.

--- comm/progress.go
+++ comm/progress.go
@@ -1,23 +1,59 @@
 package comm
 
-import "github.com/cheggaaa/pb"
+import (
+	"os"
+	"runtime"
+	"strings"
+
+	"github.com/cheggaaa/pb"
+)
 
 var bar *pb.ProgressBar
+
+type progressTheme struct {
+	BarStart string
+	BarEnd   string
+	Current  string
+	Empty    string
+}
+
+var themes = map[string]*progressTheme{
+	"unicode": {"▐", "▌", "▓", "░"},
+	"ascii":   {"|", "|", "#", "-"},
+	"cp437":   {"▐", "▌", "█", "░"},
+}
+
+func (th *progressTheme) apply(bar *pb.ProgressBar) {
+	bar.BarStart = th.BarStart
+	bar.BarEnd = th.BarEnd
+	bar.Current = th.Current
+	bar.CurrentN = th.Current
+	bar.Empty = th.Empty
+}
+
+func getCharset() string {
+	if runtime.GOOS == "windows" && os.Getenv("OS") != "CYGWIN" {
+		return "cp437"
+	}
+
+	var utf8 = ".UTF-8"
+	if strings.Contains(os.Getenv("LC_ALL"), utf8) ||
+		os.Getenv("LC_CTYPE") == "UTF-8" ||
+		strings.Contains(os.Getenv("LANG"), utf8) {
+		return "unicode"
+	}
+
+	return "ascii"
+}
 
 func StartProgress() {
 	// percentages, to the 1/100th
 	bar = pb.New64(100 * 100)
-	bar.ShowPercent = false
 	bar.ShowCounters = false
+	bar.ShowFinalTime = false
 	bar.SetMaxWidth(80)
 
-	bar.BarStart = "|"
-	bar.BarEnd = "|"
-	bar.Current = "#"
-	bar.CurrentN = "#"
-	bar.Empty = "-"
-
-	bar.ShowFinalTime = false
+	themes[getCharset()].apply(bar)
 
 	if !settings.no_progress {
 		bar.Start()
