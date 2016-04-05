46a9ca9d72648d61ecf0c7ba654c1545501f4714
Amos Wenger <fasterthanlime@gmail.com>
2016-04-05 19:40:16 +0200

    Use wharf's (de)compressors

--- compression.go
+++ compression.go
@@ -1,31 +1,6 @@
 package main
 
 import (
-	"io"
-
-	"github.com/itchio/wharf/pwr"
-
-	"gopkg.in/kothar/brotli-go.v0/dec"
-	"gopkg.in/kothar/brotli-go.v0/enc"
+	_ "github.com/itchio/wharf/compressors/cbrotli"
+	_ "github.com/itchio/wharf/decompressors/cbrotli"
 )
-
-type brotliCompressor struct{}
-
-func (bc *brotliCompressor) Apply(writer io.Writer, quality int32) (io.Writer, error) {
-	params := enc.NewBrotliParams()
-	params.SetQuality(int(quality))
-	bw := enc.NewBrotliWriter(params, writer)
-	return bw, nil
-}
-
-type brotliDecompressor struct{}
-
-func (bc *brotliDecompressor) Apply(reader io.Reader) (io.Reader, error) {
-	br := dec.NewBrotliReader(reader)
-	return br, nil
-}
-
-func init() {
-	pwr.RegisterCompressor(pwr.CompressionAlgorithm_BROTLI, &brotliCompressor{})
-	pwr.RegisterDecompressor(pwr.CompressionAlgorithm_BROTLI, &brotliDecompressor{})
-}
