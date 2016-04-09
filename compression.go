78816ad434b8c60184cf2a46a8c7f17e40f56e99
Amos Wenger <fasterthanlime@gmail.com>
2016-04-09 19:39:28 +0200

    Add gzip compressor/decompressor, doesn't seem to make binary any bigger (probably already included somewhere)

--- compression.go
+++ compression.go
@@ -3,4 +3,7 @@ package main
 import (
 	_ "github.com/itchio/wharf/compressors/cbrotli"
 	_ "github.com/itchio/wharf/decompressors/cbrotli"
+
+	_ "github.com/itchio/wharf/compressors/gzip"
+	_ "github.com/itchio/wharf/decompressors/gzip"
 )
