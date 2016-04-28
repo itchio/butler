d02c4a038b1d76bb3d535945de9f5777d04718d4
Amos Wenger <fasterthanlime@gmail.com>
2016-04-28 19:50:47 +0200

    Apply filter to zip files too

--- vendor/github.com/itchio/wharf/tlc/walk.go
+++ vendor/github.com/itchio/wharf/tlc/walk.go
@@ -139,6 +139,11 @@ func WalkZip(zr *zip.Reader, filter FilterFunc) (*Container, error) {
 		info := file.FileInfo()
 		mode := info.Mode() | ModeMask
 
+		if !filter(info) {
+			// ignore
+			continue
+		}
+
 		if info.IsDir() {
 			dirMap[dir] = mode
 		} else if mode&os.ModeSymlink > 0 {
