1cc020fedfaab7c3c4f4792fd1cc921cddb46ca4
Amos Wenger <amoswenger@gmail.com>
2016-07-08 01:14:01 +0200

    :arrow_up: wharf

--- vendor/github.com/itchio/wharf/decompressors/gbrotli/gbrotli.go
+++ vendor/github.com/itchio/wharf/decompressors/gbrotli/gbrotli.go
@@ -4,13 +4,17 @@ import (
 	"io"
 
 	"github.com/dsnet/compress/brotli"
+	"github.com/go-errors/errors"
 	"github.com/itchio/wharf/pwr"
 )
 
 type brotliDecompressor struct{}
 
 func (bc *brotliDecompressor) Apply(reader io.Reader) (io.Reader, error) {
-	br := brotli.NewReader(reader)
+	br, err := brotli.NewReader(reader, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, 1)
+	}
 	return br, nil
 }
 
