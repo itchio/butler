28972660dbadb2193fb0491e70e2e3453842e8f8
Amos Wenger <amoswenger@gmail.com>
2016-11-05 20:16:14 +0100

    :arrow_up: wharf, support redirects with ranged downloads

--- vendor/github.com/itchio/wharf/eos/option/option.go
+++ vendor/github.com/itchio/wharf/eos/option/option.go
@@ -1,6 +1,7 @@
 package option
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
@@ -19,6 +20,22 @@ func DefaultSettings() *EOSSettings {
 
 func defaultHTTPClient() *http.Client {
 	client := timeout.NewClient(time.Second*time.Duration(30), time.Second*time.Duration(15))
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		if len(via) >= 10 {
+			return errors.New("stopped after 10 redirects")
+		}
+
+		// forward initial request headers
+		// see https://github.com/itchio/itch/issues/965
+		ireq := via[0]
+		for key, values := range ireq.Header {
+			for _, value := range values {
+				req.Header.Add(key, value)
+			}
+		}
+
+		return nil
+	}
 	return client
 }
 
