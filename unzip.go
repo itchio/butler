f22db5449fbf9395de9270a350928e02807b0a2e
Amos Wenger <amos@itch.io>
2017-04-12 11:11:51 +0200

    Display speed of unzip

--- unzip.go
+++ unzip.go
@@ -1,22 +1,36 @@
 package main
 
-import "github.com/itchio/butler/comm"
-import "github.com/itchio/wharf/archiver"
+import (
+	"time"
+
+	humanize "github.com/dustin/go-humanize"
+	"github.com/itchio/butler/comm"
+	"github.com/itchio/wharf/archiver"
+)
 
 func unzip(file string, dir string, resumeFile string) {
 	comm.Opf("Extracting zip %s to %s", file, dir)
+
+	var zipUncompressedSize int64
 
 	settings := archiver.ExtractSettings{
 		Consumer:   comm.NewStateConsumer(),
 		ResumeFrom: resumeFile,
 		OnUncompressedSizeKnown: func(uncompressedSize int64) {
+			zipUncompressedSize = uncompressedSize
 			comm.StartProgressWithTotalBytes(uncompressedSize)
 		},
 	}
 
+	startTime := time.Now()
+
 	res, err := archiver.ExtractPath(file, dir, settings)
 	comm.EndProgress()
 
+	duration := time.Since(startTime)
+	bytesPerSec := float64(zipUncompressedSize) / duration.Seconds()
+
 	must(err)
-	comm.Logf("Extracted %d dirs, %d files, %d symlinks", res.Dirs, res.Files, res.Symlinks)
+	comm.Logf("Extracted %d dirs, %d files, %d symlinks, %s at %s/s", res.Dirs, res.Files, res.Symlinks,
+		humanize.IBytes(uint64(zipUncompressedSize)), humanize.IBytes(uint64(bytesPerSec)))
 }
