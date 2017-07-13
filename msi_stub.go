27f7bc0ca21aa39f11b119560c3c7ab723939bb6
Amos Wenger <amos@itch.io>
2017-07-13 18:01:46 +0200

    missing stubs for non-windows platforms

--- msi_stub.go
+++ msi_stub.go
@@ -8,6 +8,14 @@ func msiInfo(msiPath string) {
 	must(fmt.Errorf("msi-info is a windows-only command"))
 }
 
-func msiInstall(msiPath string) {
+func msiProductInfo(productCode string) {
+	must(fmt.Errorf("msi-product-info is a windows-only command"))
+}
+
+func msiInstall(msiPath string, logPath string, target string) {
 	must(fmt.Errorf("msi-install is a windows-only command"))
+}
+
+func msiUninstall(productCode string) {
+	must(fmt.Errorf("msi-uninstall is a windows-only command"))
 }
