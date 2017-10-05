12e7c399efcaa6f7f0ece23cd85c554423fdc04b
Amos Wenger <amoswenger@gmail.com>
2017-10-05 16:38:42 +0200

    Move command register calls to separate file

--- main.go
+++ main.go
@@ -13,37 +13,7 @@ import (
 	"time"
 
 	"github.com/go-errors/errors"
-	"github.com/itchio/butler/cmd/apply"
-	"github.com/itchio/butler/cmd/cave"
-	"github.com/itchio/butler/cmd/clean"
-	"github.com/itchio/butler/cmd/cp"
-	"github.com/itchio/butler/cmd/diff"
-	"github.com/itchio/butler/cmd/ditto"
-	"github.com/itchio/butler/cmd/dl"
 	"github.com/itchio/butler/cmd/elevate"
-	"github.com/itchio/butler/cmd/fetch"
-	"github.com/itchio/butler/cmd/file"
-	"github.com/itchio/butler/cmd/heal"
-	"github.com/itchio/butler/cmd/indexzip"
-	"github.com/itchio/butler/cmd/login"
-	"github.com/itchio/butler/cmd/logout"
-	"github.com/itchio/butler/cmd/ls"
-	"github.com/itchio/butler/cmd/mkdir"
-	"github.com/itchio/butler/cmd/msi"
-	"github.com/itchio/butler/cmd/pipe"
-	"github.com/itchio/butler/cmd/prereqs"
-	"github.com/itchio/butler/cmd/probe"
-	"github.com/itchio/butler/cmd/sign"
-	"github.com/itchio/butler/cmd/sizeof"
-	"github.com/itchio/butler/cmd/status"
-	"github.com/itchio/butler/cmd/untar"
-	"github.com/itchio/butler/cmd/unzip"
-	"github.com/itchio/butler/cmd/upgrade"
-	"github.com/itchio/butler/cmd/verify"
-	"github.com/itchio/butler/cmd/version"
-	"github.com/itchio/butler/cmd/walk"
-	"github.com/itchio/butler/cmd/which"
-	"github.com/itchio/butler/cmd/wipe"
 	"github.com/itchio/butler/comm"
 	"github.com/itchio/butler/filtering"
 	"github.com/itchio/butler/mansion"
@@ -181,55 +151,7 @@ func main() {
 
 func doMain(args []string) {
 	ctx := mansion.NewContext(app)
-
-	///////////////////////////
-	// Command register start
-	///////////////////////////
-
-	version.Register(ctx)
-	which.Register(ctx)
-
-	login.Register(ctx)
-	logout.Register(ctx)
-	upgrade.Register(ctx)
-
-	dl.Register(ctx)
-	cp.Register(ctx)
-	ls.Register(ctx)
-	wipe.Register(ctx)
-	sizeof.Register(ctx)
-	mkdir.Register(ctx)
-	ditto.Register(ctx)
-	file.Register(ctx)
-	probe.Register(ctx)
-
-	clean.Register(ctx)
-	walk.Register(ctx)
-
-	sign.Register(ctx)
-	diff.Register(ctx)
-	apply.Register(ctx)
-	verify.Register(ctx)
-	heal.Register(ctx)
-
-	status.Register(ctx)
-	fetch.Register(ctx)
-
-	msi.Register(ctx)
-	prereqs.Register(ctx)
-
-	unzip.Register(ctx)
-	untar.Register(ctx)
-	indexzip.Register(ctx)
-
-	pipe.Register(ctx)
-	elevate.Register(ctx)
-
-	cave.Register(ctx)
-
-	///////////////////////////
-	// Command register end
-	///////////////////////////
+	registerCommands(ctx)
 
 	app.UsageTemplate(kingpin.CompactUsageTemplate)
 	app.Flag("ignore", "Glob patterns of files to ignore when diffing").StringsVar(&filtering.IgnoredPaths)
