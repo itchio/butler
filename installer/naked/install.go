4baaa31c1958271a4088302fae194843f21dea71
Amos Wenger <amos@itch.io>
2017-11-09 16:50:40 +0100

    naked installer: create parent folder, bust ghosts

--- installer/naked/install.go
+++ installer/naked/install.go
@@ -1,18 +1,28 @@
 package naked
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/go-errors/errors"
 	"github.com/itchio/butler/cmd/ditto"
 	"github.com/itchio/butler/installer"
+	"github.com/itchio/butler/installer/bfs"
 )
 
 func (m *Manager) Install(params *installer.InstallParams) (*installer.InstallResult, error) {
 	destName := filepath.Base(params.SourcePath)
 	destAbsolutePath := filepath.Join(params.InstallFolderPath, destName)
 
-	err := ditto.Do(params.SourcePath, destAbsolutePath)
+	params.Consumer.Infof("Creating %s", params.InstallFolderPath)
+	err := os.MkdirAll(params.InstallFolderPath, 0755)
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+
+	params.Consumer.Infof("Writing %s", destAbsolutePath)
+
+	err = ditto.Do(params.SourcePath, destAbsolutePath)
 	if err != nil {
 		return nil, errors.Wrap(err, 0)
 	}
@@ -22,5 +32,15 @@ func (m *Manager) Install(params *installer.InstallParams) (*installer.InstallRe
 			destName,
 		},
 	}
+
+	err = bfs.BustGhosts(&bfs.BustGhostsParams{
+		Consumer: params.Consumer,
+		Folder:   params.InstallFolderPath,
+		NewFiles: res.Files,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+
 	return &res, nil
 }
