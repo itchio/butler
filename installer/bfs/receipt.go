b691e1d9bef1518c130dc427a82cc5ce2691f611
Amos Wenger <amos@itch.io>
2017-11-09 16:51:30 +0100

    receipt: add new fields, try writing as gzip & brotli for space saving

--- installer/bfs/receipt.go
+++ installer/bfs/receipt.go
@@ -1,16 +1,23 @@
 package bfs
 
 import (
+	"compress/gzip"
 	"encoding/json"
+	"io"
 	"os"
 	"path/filepath"
 
 	"github.com/go-errors/errors"
+	itchio "github.com/itchio/go-itchio"
+	"gopkg.in/kothar/brotli-go.v0/enc"
 )
 
 type Receipt struct {
-	Files         []string `json:"files"`
-	InstallerName string   `json:"installerName"`
+	Files         []string       `json:"files"`
+	InstallerName string         `json:"installerName"`
+	Game          *itchio.Game   `json:"game"`
+	Upload        *itchio.Upload `json:"upload"`
+	Build         *itchio.Build  `json:"build"`
 }
 
 func ReadReceipt(InstallFolder string) (*Receipt, error) {
@@ -40,7 +47,7 @@ func ReadReceipt(InstallFolder string) (*Receipt, error) {
 func (r *Receipt) WriteReceipt(InstallFolder string) error {
 	path := receiptPath(InstallFolder)
 
-	err := Mkdir(path)
+	err := Mkdir(filepath.Dir(path))
 	if err != nil {
 		return errors.Wrap(err, 0)
 	}
@@ -51,7 +58,26 @@ func (r *Receipt) WriteReceipt(InstallFolder string) error {
 	}
 	defer f.Close()
 
-	enc := json.NewEncoder(f)
+	gzf, err := os.Create(path + ".gz")
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+	defer gzf.Close()
+	gzw := gzip.NewWriter(gzf)
+	defer gzw.Close()
+
+	brf, err := os.Create(path + ".brotli")
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+	params := enc.NewBrotliParams()
+	params.SetQuality(9)
+	brw := enc.NewBrotliWriter(params, brf)
+	defer brw.Close()
+
+	mw := io.MultiWriter(f, gzw, brw)
+
+	enc := json.NewEncoder(mw)
 	err = enc.Encode(r)
 	if err != nil {
 		return errors.Wrap(err, 0)
