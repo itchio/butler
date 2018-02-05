63dbe9cfc13ebbc48e71df90dc3388061157e981
Amos Wenger <amos@itch.io>
2018-02-05 15:27:47 +0100

    launch: pass temp dir (and clean it up), format non-zero exit code & ignore if app has been running for 2s. Closes https://github.com/itchio/itch/issues/1688

--- cmd/launch/launchers/native/native.go
+++ cmd/launch/launchers/native/native.go
@@ -5,10 +5,15 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/go-errors/errors"
 	"github.com/itchio/butler/buse"
 	"github.com/itchio/butler/cmd/launch"
+	"github.com/itchio/butler/cmd/wipe"
 )
 
 func Register() {
@@ -22,6 +27,7 @@ var _ launch.Launcher = (*Launcher)(nil)
 func (l *Launcher) Do(params *launch.LauncherParams) error {
 	ctx := params.Ctx
 	conn := params.Conn
+	consumer := params.Consumer
 	installFolder := params.ParentParams.InstallFolder
 
 	cwd := installFolder
@@ -40,26 +46,90 @@ func (l *Launcher) Do(params *launch.LauncherParams) error {
 	cmd := exec.Command(params.FullTargetPath, params.Args...)
 	cmd.Dir = cwd
 
-	// TODO: sanitize environment somewhat?
-	env := os.Environ()
+	envMap := make(map[string]string)
 	for k, v := range params.Env {
-		env = append(env, fmt.Sprintf("%s=%s", k, v))
+		envMap[k] = v
 	}
-	cmd.Env = env
+
+	// give the app its own temporary directory
+	tempDir := filepath.Join(params.ParentParams.InstallFolder, ".itch", "temp")
+	err = os.MkdirAll(tempDir, 0755)
+	if err != nil {
+		consumer.Warnf("Could not make temporary directory: %s", err.Error())
+	} else {
+		defer wipe.Do(consumer, tempDir)
+		envMap["TMP"] = tempDir
+		envMap["TEMP"] = tempDir
+		consumer.Infof("Giving app temp dir (%s)", tempDir)
+	}
+
+	var envKeys []string
+	for k := range envMap {
+		envKeys = append(envKeys, k)
+	}
+	consumer.Infof("Environment variables passed: %s", strings.Join(envKeys, ", "))
+
+	// TODO: sanitize environment somewhat?
+	envBlock := os.Environ()
+	for k, v := range envMap {
+		envBlock = append(envBlock, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	cmd.Env = envBlock
 
 	err = cmd.Start()
 	if err != nil {
 		return errors.Wrap(err, 0)
 	}
 
-	conn.Notify(ctx, "LaunchRunning", &buse.LaunchRunningNotification{})
+	startTime := time.Now()
 
-	err = cmd.Wait()
+	conn.Notify(ctx, "LaunchRunning", &buse.LaunchRunningNotification{})
+	exitCode, err := waitCommand(cmd)
+	conn.Notify(ctx, "LaunchExited", &buse.LaunchExitedNotification{})
+
 	if err != nil {
 		return errors.Wrap(err, 0)
 	}
 
-	conn.Notify(ctx, "LaunchExited", &buse.LaunchExitedNotification{})
+	runDuration := time.Since(startTime)
+
+	if exitCode != 0 {
+		var signedExitCode = int64(exitCode)
+		if runtime.GOOS == "windows" {
+			// Windows uses 32-bit unsigned integers as exit codes,[11] although the
+			// command interpreter treats them as signed.[12] If a process fails
+			// initialization, a Windows system error code may be returned.[13][14]
+			signedExitCode = int64(int32(signedExitCode))
+
+			// The line above turns `4294967295` into -1
+		}
+
+		exeName := filepath.Base(params.FullTargetPath)
+		msg := fmt.Sprintf("Exit code 0x%x (%d) for (%s)", uint32(exitCode), signedExitCode, exeName)
+		consumer.Warnf(msg)
+
+		if runDuration.Seconds() > 2 {
+			consumer.Warnf("That's after running for %s, ignoring non-zero exit code", runDuration)
+		} else {
+			return errors.New(msg)
+		}
+	}
 
 	return nil
+}
+
+func waitCommand(cmd *exec.Cmd) (int, error) {
+	err := cmd.Wait()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			if status, ok := exitError.Sys().(syscall.WaitStatus); ok {
+				return status.ExitStatus(), nil
+			}
+		}
+
+		return 127, err
+	}
+
+	return 0, nil
 }
