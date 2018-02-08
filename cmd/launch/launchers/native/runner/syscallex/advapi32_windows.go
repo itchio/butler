d36dce8e7f1ce7fcb231a6f90bf2ff999810717f
Amos Wenger <amos@itch.io>
2018-02-08 20:42:40 +0100

    add Logon, Impersonate, Revert, GetFolderPath

--- cmd/launch/launchers/native/runner/syscallex/advapi32_windows.go
+++ cmd/launch/launchers/native/runner/syscallex/advapi32_windows.go
@@ -13,10 +13,23 @@ const (
 	LOGON_CREDENTIALS_ONLY = 2
 )
 
+// logon type
+const (
+	LOGON32_LOGON_INTERACTIVE = 2
+)
+
+// logon provider
+const (
+	LOGON32_PROVIDER_DEFAULT = 0
+)
+
 var (
 	modadvapi32 = windows.NewLazySystemDLL("advapi32.dll")
 
 	procCreateProcessWithLogonW = modadvapi32.NewProc("CreateProcessWithLogonW")
+	procLogonUserW              = modadvapi32.NewProc("LogonUserW")
+	procImpersonateLoggedOnUser = modadvapi32.NewProc("ImpersonateLoggedOnUser")
+	procRevertToSelf            = modadvapi32.NewProc("RevertToSelf")
 )
 
 func CreateProcessWithLogon(
@@ -47,6 +60,69 @@ func CreateProcessWithLogon(
 		uintptr(unsafe.Pointer(startupInfo)),
 		uintptr(unsafe.Pointer(outProcInfo)),
 		0,
+	)
+	if r1 == 0 {
+		if e1 != 0 {
+			err = e1
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func LogonUser(
+	username *uint16,
+	domain *uint16,
+	password *uint16,
+	logonType uint32,
+	logonProvider uint32,
+	outToken *syscall.Handle,
+) (err error) {
+	r1, _, e1 := syscall.Syscall6(
+		procLogonUserW.Addr(),
+		6,
+		uintptr(unsafe.Pointer(username)),
+		uintptr(unsafe.Pointer(domain)),
+		uintptr(unsafe.Pointer(password)),
+		uintptr(logonType),
+		uintptr(logonProvider),
+		uintptr(unsafe.Pointer(outToken)),
+	)
+	if r1 == 0 {
+		if e1 != 0 {
+			err = e1
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func ImpersonateLoggedOnUser(
+	token syscall.Handle,
+) (err error) {
+	r1, _, e1 := syscall.Syscall(
+		procImpersonateLoggedOnUser.Addr(),
+		1,
+		uintptr(token),
+		0, 0,
+	)
+	if r1 == 0 {
+		if e1 != 0 {
+			err = e1
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func RevertToSelf() (err error) {
+	r1, _, e1 := syscall.Syscall(
+		procRevertToSelf.Addr(),
+		0,
+		0, 0, 0,
 	)
 	if r1 == 0 {
 		if e1 != 0 {
