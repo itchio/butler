0f039b339ab9dc81cd5aee1b926bdf88936b4876
Amos Wenger <amos@itch.io>
2018-02-08 20:58:53 +0100

    set up environment, not working for some reason.

--- cmd/launch/launchers/native/runner/winsandbox_windows.go
+++ cmd/launch/launchers/native/runner/winsandbox_windows.go
@@ -3,6 +3,9 @@
 package runner
 
 import (
+	"fmt"
+	"syscall"
+
 	"github.com/go-errors/errors"
 	"github.com/itchio/butler/cmd/launch/launchers/native/runner/execas"
 	"github.com/itchio/butler/cmd/launch/launchers/native/runner/syscallex"
@@ -75,12 +78,75 @@ func (wr *winsandboxRunner) Run() error {
 
 	consumer.Infof("Running as user (%s)", wr.username)
 
+	var token syscall.Handle
+	err = syscallex.LogonUser(
+		syscall.StringToUTF16Ptr(wr.username),
+		syscall.StringToUTF16Ptr("."),
+		syscall.StringToUTF16Ptr(wr.password),
+		syscallex.LOGON32_LOGON_INTERACTIVE,
+		syscallex.LOGON32_PROVIDER_DEFAULT,
+		&token,
+	)
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+
+	_, err = syscall.GetEnvironmentStrings()
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+
+	err = syscallex.ImpersonateLoggedOnUser(token)
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+
+	profileDir, err := syscallex.SHGetFolderPath(
+		0,
+		syscallex.CSIDL_PROFILE|syscallex.CSIDL_FLAG_CREATE,
+		0,
+		syscallex.SHGFP_TYPE_CURRENT,
+	)
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+
+	appDataDir, err := syscallex.SHGetFolderPath(
+		0,
+		syscallex.CSIDL_APPDATA|syscallex.CSIDL_FLAG_CREATE,
+		0,
+		syscallex.SHGFP_TYPE_CURRENT,
+	)
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+
+	localAppDataDir, err := syscallex.SHGetFolderPath(
+		0,
+		syscallex.CSIDL_LOCAL_APPDATA|syscallex.CSIDL_FLAG_CREATE,
+		0,
+		syscallex.SHGFP_TYPE_CURRENT,
+	)
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+
+	err = syscallex.RevertToSelf()
+	if err != nil {
+		return errors.Wrap(err, 0)
+	}
+
+	env := params.Env
+	env = append(env, fmt.Sprintf("userprofile=%s", profileDir))
+	env = append(env, fmt.Sprintf("appdata=%s", appDataDir))
+	env = append(env, fmt.Sprintf("localappdata=%s", localAppDataDir))
+
 	cmd := execas.CommandContext(params.Ctx, params.FullTargetPath, params.Args...)
 	cmd.Username = wr.username
 	cmd.Domain = "."
 	cmd.Password = wr.password
 	cmd.Dir = params.Dir
-	cmd.Env = params.Env
+	cmd.Env = env
 	cmd.Stdout = params.Stdout
 	cmd.Stderr = params.Stderr
 	cmd.SysProcAttr = &syscallex.SysProcAttr{
