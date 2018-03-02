a6da827d65d3bafe7f6d3924d312358fbd949bda
Amos Wenger <amos@itch.io>
2018-03-02 10:24:38 +0100

    Document diff a bit

--- endpoints/fetch/diff.go
+++ endpoints/fetch/diff.go
@@ -11,6 +11,7 @@ import (
 )
 
 func diff(tx *gorm.DB, consumer *state.Consumer, inputIface interface{}) error {
+	// inputIFace is a `[]interface{}`
 	input := reflect.ValueOf(inputIface)
 	if input.Kind() != reflect.Slice {
 		return errors.New("diff needs a slice")
@@ -21,6 +22,8 @@ func diff(tx *gorm.DB, consumer *state.Consumer, inputIface interface{}) error {
 		return nil
 	}
 
+	// we're trying to make fresh a `[]*SomeModel` slice, so
+	// that gorm can get annotation information from SomeModel struct annotations
 	first := input.Index(0).Elem()
 	fresh := reflect.MakeSlice(reflect.SliceOf(first.Type()), input.Len(), input.Len())
 	for i := 0; i < input.Len(); i++ {
@@ -28,8 +31,8 @@ func diff(tx *gorm.DB, consumer *state.Consumer, inputIface interface{}) error {
 		fresh.Index(i).Set(record)
 	}
 
+	// use gorm facilities to find the primary keys
 	scope := tx.NewScope(first.Interface())
-
 	modelName := scope.GetModelStruct().ModelType.Name()
 	var pkColumns []string
 	fs := scope.Fields()
@@ -41,16 +44,19 @@ func diff(tx *gorm.DB, consumer *state.Consumer, inputIface interface{}) error {
 
 	consumer.Infof("Persisting %d records for %s, primary keys: (%s)", fresh.Len(), modelName, strings.Join(pkColumns, ", "))
 
+	// this will happen for associations, we should have another codepath for that
 	if len(pkColumns) != 1 {
 		return fmt.Errorf("Have %d primary keys, don't know what to do", len(pkColumns))
 	}
 
 	pkColumn := pkColumns[0]
 
+	// record should be a *SomeModel, we're effecctively doing (*record).<pkColumn>
 	getPk := func(record reflect.Value) interface{} {
 		return reflect.Indirect(record).FieldByName(pkColumn).Interface()
 	}
 
+	// collect primary key values for all of input
 	var pks []interface{}
 	for i := 0; i < fresh.Len(); i++ {
 		record := fresh.Index(i)
@@ -59,6 +65,9 @@ func diff(tx *gorm.DB, consumer *state.Consumer, inputIface interface{}) error {
 
 	var err error
 
+	// retrieve cached items in a []*SomeModel
+	// for some reason, reflect.New returns a &[]*SomeModel instead,
+	// I'm guessing slices can't be interfaces, but pointers to slices can?
 	cacheAddr := reflect.New(fresh.Type())
 	err = tx.Where(fmt.Sprintf("%s in (?)", pkColumn), pks).Find(cacheAddr.Interface()).Error
 	if err != nil {
@@ -67,12 +76,15 @@ func diff(tx *gorm.DB, consumer *state.Consumer, inputIface interface{}) error {
 
 	cache := reflect.Indirect(cacheAddr)
 
+	// index cached items by their primary key
+	// so we can look them up in O(1) when comparing
 	cacheByPK := make(map[interface{}]reflect.Value)
 	for i := 0; i < cache.Len(); i++ {
 		record := cache.Index(i)
 		cacheByPK[getPk(record)] = record
 	}
 
+	// compare cached records with fresh records
 	var insertPKs []interface{}
 	var updatePKs []interface{}
 
@@ -80,6 +92,9 @@ func diff(tx *gorm.DB, consumer *state.Consumer, inputIface interface{}) error {
 		frec := fresh.Index(i)
 		pk := getPk(frec)
 		if crec, ok := cacheByPK[pk]; ok {
+			// frec and crec are *SomeModel, but `RecordEqual` ignores pointer
+			// equality - we want to compare the contents of the struct
+			// so we indirect to SomeModel here.
 			ifrec := reflect.Indirect(frec).Interface()
 			icrec := reflect.Indirect(crec).Interface()
 			if !RecordEqual(ifrec, icrec) {
