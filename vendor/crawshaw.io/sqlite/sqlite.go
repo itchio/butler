8701bb39ecad395b35727420832686fdcfbd5f7a
Amos Wenger <amos@itch.io>
2018-06-03 19:46:17 +0200

    Vendor sqlite fork to work around https://github.com/crawshaw/sqlite/issues/15

--- vendor/crawshaw.io/sqlite/sqlite.go
+++ vendor/crawshaw.io/sqlite/sqlite.go
@@ -27,6 +27,7 @@ package sqlite
 // #cgo CFLAGS: -DSQLITE_USE_ALLOCA
 // #cgo CFLAGS: -DSQLITE_ENABLE_COLUMN_METADATA
 // #cgo linux LDFLAGS: -ldl -lm
+// #cgo linux CFLAGS: -std=c99
 // #cgo openbsd LDFLAGS: -lm
 // #cgo openbsd CFLAGS: -std=c99
 //
