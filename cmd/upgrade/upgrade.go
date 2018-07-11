b680aac30f9df270b1b32e5b288b94d90da1c518
Amos Wenger <amos@itch.io>
2018-07-11 23:47:17 +0200

    Closes #162 (use subdirectory of exec dir so os.Rename won't fail, and also rollback if we failed)

--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -2,7 +2,6 @@ package upgrade
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -13,8 +12,9 @@ import (
 )
 
 var args = struct {
-	head   *bool
-	stable *bool
+	head   bool
+	stable bool
+	force  bool
 }{}
 
 func Register(ctx *mansion.Context) {
@@ -22,15 +22,16 @@ func Register(ctx *mansion.Context) {
 	cmd := ctx.App.Command("upgrade", "Upgrades butler to the latest version").Alias("ugprade").Alias("update")
 	ctx.Register(cmd, do)
 
-	args.head = cmd.Flag("head", "Force bleeding-edge version").Bool()
-	args.stable = cmd.Flag("stable", "Force stable version").Bool()
+	cmd.Flag("head", "Force bleeding-edge version").BoolVar(&args.head)
+	cmd.Flag("stable", "Force stable version").BoolVar(&args.stable)
+	cmd.Flag("force", "Force upgrade, even when using self-built butler").BoolVar(&args.force)
 }
 
 func do(ctx *mansion.Context) {
-	ctx.Must(Do(ctx, *args.head, *args.stable))
+	ctx.Must(Do(ctx, args.head, args.stable, args.force))
 }
 
-func Do(ctx *mansion.Context, head bool, stable bool) error {
+func Do(ctx *mansion.Context, head bool, stable bool, force bool) error {
 	variant := ctx.CurrentVariant()
 	if head {
 		variant = mansion.VersionVariantHead
@@ -43,6 +44,11 @@ func Do(ctx *mansion.Context, head bool, stable bool) error {
 	vinfo, err := ctx.QueryLatestVersion(variant)
 	if err != nil {
 		return fmt.Errorf("Version check failed: %s", err.Error())
+	}
+
+	if vinfo.Current.Name == "" && !force {
+		comm.Warnf("Refusing to upgrade self-built butler without --force")
+		return nil
 	}
 
 	if vinfo.Latest.Equal(vinfo.Current) {
@@ -62,14 +68,9 @@ func Do(ctx *mansion.Context, head bool, stable bool) error {
 }
 
 func applyUpgrade(ctx *mansion.Context, vinfo *mansion.VersionCheckResult) error {
+	consumer := comm.NewStateConsumer()
 	before := vinfo.Current
 	after := vinfo.Latest
-
-	updateDir, err := ioutil.TempDir("", "butler-self-upgrade")
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	defer os.RemoveAll(updateDir)
 
 	execPath, err := os.Executable()
 	if err != nil {
@@ -78,34 +79,84 @@ func applyUpgrade(ctx *mansion.Context, vinfo *mansion.VersionCheckResult) error
 
 	execDir := filepath.Dir(execPath)
 
-	archiveURL := fmt.Sprintf("%s/%s/.zip", ctx.UpdateBaseURL(after.Variant), after.Name)
-	comm.Opf("%s", archiveURL)
+	updateDir := filepath.Join(execDir, ".butler-self-upgrade")
+	err = os.MkdirAll(updateDir, 0755)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer os.RemoveAll(updateDir)
 
+	archiveURL := fmt.Sprintf("%s/%s/.zip", ctx.UpdateBaseURL(after.Variant), after.Name)
+	consumer.Opf("%s", archiveURL)
+
+	comm.StartProgress()
 	extractRes, err := boar.SimpleExtract(&boar.SimpleExtractParams{
 		ArchivePath:       archiveURL,
 		DestinationFolder: updateDir,
-		Consumer:          comm.NewStateConsumer(),
+		Consumer:          consumer,
 	})
+	comm.EndProgress()
 	if err != nil {
 		return err
 	}
 
-	var oldPaths []string
+	type Item struct {
+		SourcePath string
+		DestPath   string
+		BackupPath string
+	}
+
+	var items []Item
 
 	for _, entry := range extractRes.Entries {
 		srcPath := filepath.Join(updateDir, entry.CanonicalPath)
 		dstPath := filepath.Join(execDir, entry.CanonicalPath)
 		oldPath := dstPath + ".old"
 
-		oldPaths = append(oldPaths, oldPath)
-		os.Rename(dstPath, oldPath)
-		os.Rename(srcPath, dstPath)
+		items = append(items, Item{
+			SourcePath: srcPath,
+			DestPath:   dstPath,
+			BackupPath: oldPath,
+		})
 	}
 
-	for _, oldPath := range oldPaths {
-		err = os.Remove(oldPath)
+	backup := func() {
+		for _, item := range items {
+			os.Rename(item.DestPath, item.BackupPath)
+		}
 	}
 
-	comm.Statf("Upgraded butler from %s to %s. Have a nice day!", before, after)
+	apply := func() error {
+		for _, item := range items {
+			err := os.Rename(item.SourcePath, item.DestPath)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	rollback := func() {
+		for _, item := range items {
+			os.Rename(item.BackupPath, item.DestPath)
+		}
+	}
+
+	cleanup := func() {
+		for _, item := range items {
+			os.Remove(item.BackupPath)
+		}
+	}
+
+	defer cleanup()
+
+	backup()
+	err = apply()
+	if err != nil {
+		rollback()
+		return errors.Wrap(err, "Self-upgrade failed")
+	}
+
+	consumer.Statf("Upgraded butler from %s to %s. Have a nice day!", before, after)
 	return nil
 }
