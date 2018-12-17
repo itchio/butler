518903deac3a6c7bdc60ee565d2ebb47ee24bf30
Amos Wenger <amos@itch.io>
2018-12-17 13:40:05 +0100

    Return the right error in push/create_files

--- cmd/push/create_files.go
+++ cmd/push/create_files.go
@@ -1,11 +1,10 @@
 package push
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/itchio/butler/comm"
-	"github.com/itchio/go-itchio"
+	itchio "github.com/itchio/go-itchio"
 	"github.com/pkg/errors"
 )
 
@@ -16,7 +15,7 @@ type createBothFilesResponse struct {
 
 func createBothFiles(client *itchio.Client, buildID int64) (*createBothFilesResponse, error) {
 	createFile := func(buildType itchio.BuildFileType, result **itchio.CreateBuildFileResponse) error {
-		res, err := client.CreateBuildFile(itchio.CreateBuildFileParams{
+		buildFileRes, err := client.CreateBuildFile(itchio.CreateBuildFileParams{
 			BuildID:        buildID,
 			Type:           buildType,
 			SubType:        itchio.BuildFileSubTypeDefault,
@@ -25,34 +24,34 @@ func createBothFiles(client *itchio.Client, buildID int64) (*createBothFilesResp
 		if err != nil {
 			return errors.WithMessage(err, "creating build file on server")
 		}
-		comm.Debugf("Created %s build file: %+v", buildType, res.File)
+		comm.Debugf("Created %s build file: %+v", buildType, buildFileRes.File)
 
 		// TODO: resumable upload session creation sounds like it belongs in an external lib, go-itchio maybe?
-		req, reqErr := http.NewRequest("POST", res.File.UploadURL, nil)
-		if reqErr != nil {
+		req, err := http.NewRequest("POST", buildFileRes.File.UploadURL, nil)
+		if err != nil {
 			return errors.WithMessage(err, "getting resumable upload session parameters")
 		}
 
 		req.ContentLength = 0
 
-		for k, v := range res.File.UploadHeaders {
+		for k, v := range buildFileRes.File.UploadHeaders {
 			req.Header.Add(k, v)
 		}
 
-		gcsRes, gcsErr := client.HTTPClient.Do(req)
-		if gcsErr != nil {
+		gcsRes, err := client.HTTPClient.Do(req)
+		if err != nil {
 			return errors.WithMessage(err, "creating resumable upload session")
 		}
 
 		if gcsRes.StatusCode != 201 {
-			return errors.WithMessage(err, fmt.Sprintf("while creating resumable upload session, got HTTP %d", gcsRes.StatusCode))
+			return errors.Errorf("while creating resumable upload session, got HTTP %d", gcsRes.StatusCode)
 		}
 
-		res.File.UploadHeaders = nil
-		res.File.UploadURL = gcsRes.Header.Get("Location")
-		comm.Debugf("Started resumable upload session %s", res.File.UploadURL)
+		buildFileRes.File.UploadHeaders = nil
+		buildFileRes.File.UploadURL = gcsRes.Header.Get("Location")
+		comm.Debugf("Started resumable upload session %s", buildFileRes.File.UploadURL)
 
-		*result = res
+		*result = buildFileRes
 		return nil
 	}
 
