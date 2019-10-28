7c80e9d118bbce27a14bf7bb50aec774752db3e2
Amos Wenger <amoswenger@gmail.com>
2019-10-28 20:32:24 +0100

    Stop deploying to ibrew

--- scripts/ci-deploy-ibrew.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-
-# upload to legacy ibrew repo
-gsutil -m cp -r -a public-read ibrew/* gs://dl.itch.ovh/butler/
--- scripts/ci-package.sh
+++ scripts/ci-package.sh
@@ -29,34 +29,6 @@ file built/$TARGET
 # run integration tests
 go test -v ./butlerd/integrate --butlerPath=$PWD/built/$TARGET
 
-(cd built/ && 7za a ../butler.7z *)
-(cd built/ && 7za a ../butler.zip *)
-(cd built/ && 7za a ../butler.gz $TARGET)
-
-# set up a file hierarchy that ibrew can consume, ie:
-#
-# - windows-amd64
-#   - LATEST
-#   - v0.11.0
-#     - butler.7z
-#     - butler.gz
-#     - butler.exe
-#     - SHA1SUMS
-#     - SHA256SUMS
-
-IBREW_DIR="ibrew/$CI_OS-$CI_ARCH"
-mkdir -p $IBREW_DIR/$CI_VERSION
-mv butler.7z $IBREW_DIR/$CI_VERSION
-mv butler.gz $IBREW_DIR/$CI_VERSION
-mv butler.zip $IBREW_DIR/$CI_VERSION
-cp built/* $IBREW_DIR/$CI_VERSION
-
-(cd $IBREW_DIR/$CI_VERSION && sha1sum * > SHA1SUMS && sha256sum * > SHA256SUMS)
-
-if [ -n "$CI_BUILD_TAG" ]; then
-  echo $CI_VERSION > $IBREW_DIR/LATEST
-fi
-
 # set up a file hierarchy we can push with butler, ie.
 #
 # - windows-amd64
