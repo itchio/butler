2fbde7803f02e9898aae01f8e09824dbca6fb3cc
Amos Wenger <amoswenger@gmail.com>
2019-10-28 18:32:18 +0100

    Make sure go.mod doesn't contain any replace directives

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -4,6 +4,19 @@ go version
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 
+grep "replace" go.mod
+if [[ $? == 0 ]]; then
+    set +x
+    echo "======================================================="
+    echo "=                      NOPE                           ="
+    echo "======================================================="
+    echo "= go.mod: should not have any replace directives      ="
+    echo "=                                                     ="
+    echo "= signed: your friendly neighborhood automated check  ="
+    echo "======================================================="
+    exit 1
+fi
+
 go test -v -race -cover -coverprofile=coverage.txt ./...
 
 curl -s https://codecov.io/bash | bash
